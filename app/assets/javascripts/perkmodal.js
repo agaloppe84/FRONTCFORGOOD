8b4dd82cc7a3b07258bb937aac59b57c1fb50b40
aurelien galoppe <agaloppe@me.com>
2016-11-04 20:15:40 +0100

    pseudo modal done

---
+++ app/assets/javascripts/perkmodal.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".perk-pseudo-modal-link").click(function() {
+    $(".perk-modal").removeClass("hidden");
+    });
+});
+
+
+$(document).ready(function() {
+    $('.perk-modal-close').click(function() {
+    $(".perk-modal").addClass('hidden');
+    });
+});
